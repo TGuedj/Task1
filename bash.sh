5d1dffe73f2e27f2529e0f1bc7ff6194f9118f71
TGuedj <156042593+TGuedj@users.noreply.github.com>
2024-07-05 19:57:57 +0300

    Edited bash.sh

--- bash.sh
+++ bash.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+!/bin/bash
 
-# Send a request to the Flask app and get the joke
 response=$(curl -s http://localhost:5000/)
 joke=$(echo $response | jq -r '.value')
 
-# Count the number of words in the joke
 word_count=$(echo $joke | wc -w)
 
-# Print the word count
 echo "The joke has $word_count words."
